8663d5325f560134eec41b3f14eda4137fad0f68
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-04-29 12:47:23 +0530

    Time: 75 ms (20.28%), Space: 27.3 MB (8.85%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        multiset<vector<int>> ms(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        while(ms.size() > 1) {
+            auto it = ms.begin();
+            auto f = *it;
+            advance(it, 1);
+            auto s = *it;
+            if(s[0] <= f[1]) {
+                ms.erase(ms.find(f));
+                ms.erase(ms.find(s));
+                vector<int> val = {f[0], max(s[1], f[1])};
+                ms.insert(val);
+            }
+            else {
+                ans.push_back(f);
+                ms.erase(ms.find(f));
+            }
+        }
+        ans.push_back(*ms.begin());
+        return ans;
+    }
+};
