f49530de2bef5ff00bc106025a0209c83af40f75
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-09 23:05:59 +0530

    Time: 6 ms (44.70%), Space: 6.4 MB (56.49%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,45 +1,22 @@
-int MOD = 1e9 + 7;
+vector<int> zFunction(string s, int n) {
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i <= r) {
+            z[i] = min(r - i + 1, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[z[i] + i]) {
+            z[i]++;
+        }
+        if(i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
 class Solution {
 public:
-    int power(long long x, int n) {
-        long long res = 1;
-        while(n > 0) {
-            if(n & 1) {
-                res *= x;
-                res %= MOD;
-            }
-            x *= x;
-            x %= MOD;
-            n >>= 1;
-        }
-        res %= MOD;
-        return res;
-    }
-    int inv(long long x) {
-        return power(x, MOD - 2);
-    }
-    vector<int> computeHash(string &s) {
-        vector<int> pref;
-        int p = 31;
-        long long pw = 0, hash = 0;
-        for(char c: s) {
-            hash += (c - 'a' + 1) * 1LL * power(p, pw);
-            hash %= MOD;
-            pref.push_back(hash);
-            pw++;
-        }
-        return pref;
-    }
-    int computeFullHash(string &s) {
-        int p = 31;
-        long long pw = 0, hash = 0;
-        for(char c: s) {
-            hash += (c - 'a' + 1) * 1LL * power(p, pw);
-            hash %= MOD;
-            pw++;
-        }
-        return (int)hash;
-    }
     int strStr(string s, string pat) {
         if(pat.empty()) {
             return 0;
@@ -47,26 +24,14 @@ public:
         if(pat.size() > s.size()) {
             return -1;
         }
-        int patHash = computeFullHash(pat);
-        vector<int> h = computeHash(s);
-        int l = 0, r = pat.size() - 1, n = s.size();
-        while(r < n) {
-            long long hashlr = 0;
-            if(l == 0) {
-                hashlr = (h[r] + MOD) % MOD;
-                hashlr = (hashlr * inv(power(31, l)));
-                hashlr %= MOD;
+        string match = pat + '$' + s;
+        int n = s.size();
+        int m = pat.size();
+        vector<int> z = zFunction(match, n + m + 1);
+        for(int i = m; i <= n + m; i++) {
+            if(z[i] == m) {
+                return i - m - 1;
             }
-            else {
-                hashlr = (h[r] - h[l - 1] + MOD) % MOD;
-                hashlr = (hashlr * inv(power(31, l)));
-                hashlr %= MOD;
-            }
-            if(hashlr == patHash) {
-                return l;
-            }
-            l++;
-            r++;
         }
         return -1;
     }
