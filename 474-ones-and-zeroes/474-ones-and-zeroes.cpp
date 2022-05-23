096b5af324a40108f854d2f3befcd7aebdc88343
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-23 09:51:10 +0530

    Time: 713 ms (17.00%), Space: 9.8 MB (70.82%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        // dp[i][j] = size of largest subset which has atmost i zeroes and j ones if i take/not take the current string
+        //dp[0][i] = all elements with only ones with total <= i
+        // dp[i][0] = all elements with only zeroes with total <= i
+        // dp[0][0] = 0
+        // dp[i][j] = max(dp[i][j], dp[i - zeroes_in_string][j - ones_in_string]
+        for(auto &x: strs) {
+            for(int i = m; i >= 0; i--) {
+                for(int j = n; j >= 0; j--) {
+                    int zeroes = count(x.begin(), x.end(), '0');
+                    int ones = count(x.begin(), x.end(), '1');
+                    if(i - zeroes >= 0 && j - ones >= 0) {
+                        dp[i][j] = max(dp[i][j], dp[i - zeroes][j - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
