1af142640567d46aa9ec46f2377c460000e2c294
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-25 00:06:38 +0530

    Time: 47 ms (10.57%), Space: 21.4 MB (69.28%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n = nums.size();
-        vector<int> odd, even;
-        for(auto &x: nums) {
-            if(x & 1) {
-                odd.push_back(x);
-            }
-            else {
-                even.push_back(x);
-            }
-        }
-        vector<int> ans;
         for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) {
-                ans.push_back(even.back());
-                even.pop_back();
-            }
-            else {
-                ans.push_back(odd.back());
-                odd.pop_back();
+            if(nums[i] % 2 != i % 2) {
+                int j = i + 1;
+                while(j < n && nums[j] % 2 != i % 2) {
+                    j++;
+                }
+                swap(nums[i], nums[j]);
             }
         }
-        return ans;
+        return nums;
     }
 };
