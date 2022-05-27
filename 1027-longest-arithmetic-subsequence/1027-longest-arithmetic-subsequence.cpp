61d24af9b661279a89a79937c2eeba11f4456101
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-27 13:11:49 +0530

    Time: 375 ms (54.77%), Space: 141.8 MB (38.07%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -6,15 +6,13 @@ public:
         // dp[i][j] = max(dp[i][j], dp[k][j] + 1)
         int n = (int)nums.size();
         vector<vector<int>> dp(n + 1, vector<int>(1005, 1));
+        int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < i; j++) {
                 int d = nums[i] - nums[j] + 500;
                 dp[i][d] = max(dp[i][d], dp[j][d] + 1); 
+                ans = max(ans, dp[i][d]);
             }
-        }
-        int ans = 0;
-        for(auto &x: dp) {
-            ans = max(ans, *max_element(x.begin(), x.end()));
         }
         return ans;
     }
