d0c327c0b6bb845cb45e7726f5350a32371e5b5e
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-28 15:02:05 +0530

    Time: 65 ms (17.25%), Space: 21.2 MB (9.41%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int recurse(vector<vector<vector<int>>> &dp, vector<int> &prices, int ind, bool buy, int k) {
+        if(k == 0) {
+            return 0;
+        }
+        if(ind == (int)prices.size()) {
+            return 0;
+        }
+        if(dp[ind][k][buy] != -1) {
+            return dp[ind][k][buy];
+        }
+        if(buy) {
+            dp[ind][k][buy] = max(recurse(dp, prices, ind + 1, buy ^ 1, k) - prices[ind], recurse(dp, prices, ind + 1, buy, k));
+        }
+        else {
+            dp[ind][k][buy] = max(recurse(dp, prices, ind + 1, buy ^ 1, k - 1) + prices[ind], recurse(dp, prices, ind + 1, buy, k));
+        }
+        return dp[ind][k][buy];
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(2, -1)));
+        return recurse(dp, prices, 0, 1, k);
+    }
+};
