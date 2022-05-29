1799fa91ec9eb6ba87ba5835278aeca296330579
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-29 13:38:09 +0530

    Time: 48 ms (88.18%), Space: 15.6 MB (79.69%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -8,7 +8,6 @@ public:
                 bitmasks[i] |= (1LL << (c - 'a'));
             }
         }
-        cout << endl;
         int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
