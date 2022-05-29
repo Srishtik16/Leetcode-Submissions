a9406661dc432a9b312961564e7cc6f3b0ba82d0
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-29 13:38:02 +0530

    Time: 58 ms (73.14%), Space: 15.6 MB (89.46%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        vector<int> bitmasks(n);
+        for(int i = 0; i < n; i++) {
+            for(char c: words[i]) {
+                bitmasks[i] |= (1LL << (c - 'a'));
+            }
+        }
+        cout << endl;
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if((bitmasks[i] & bitmasks[j]) == 0) {
+                    ans = max(ans, (int)words[i].size() * (int)words[j].size());
+                }
+            }
+        }
+        return ans;
+    }
+};
