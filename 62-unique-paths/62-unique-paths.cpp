e2920e557ddc8057181134eba65bd8e266cd5039
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-31 22:53:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.10%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,20 +1,21 @@
-using ll = unsigned long long;
 class Solution {
 public:
-    int uniquePaths(int rows, int columns) {
-        int ptr = 1;
-        ll ans = 1;
-        for(int i = rows + columns - 2; i >= 1; i--) {
-            ans *= i;
-            ans /= ptr;
-            ptr++;
-            if(ptr == min(rows, columns)) {
-                break;
+    int uniquePaths(int m, int n) {
+        swap(m, n);
+        vector<long long> dp(m + 1, 0);
+        for(int i = 0; i < n; i++) {
+            vector<long long> new_dp(m + 1, 0);
+            if(i == 0) {
+                new_dp[0] = 1;
             }
+            else {
+                new_dp[0] = dp[0];
+            }
+            for(int j = 1; j < m; j++) {
+                new_dp[j] = new_dp[j - 1] + dp[j];
+            }
+            dp = new_dp;
         }
-        if(ans == 0) {
-            ans = 1;
-        }
-        return (int)ans;
+        return dp[m - 1];
     }
 };
