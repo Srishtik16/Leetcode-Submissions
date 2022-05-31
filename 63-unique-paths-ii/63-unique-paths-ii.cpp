9c70863e35079448f86a29a4c9c10a6e74b6f8e3
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-05-31 23:01:42 +0530

    Time: 13 ms (7.86%), Space: 7.8 MB (63.00%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,31 +3,32 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1));
-        dp[0][0] = grid[0][0] == 0;
+        vector<int> dp(m + 1);
         for(int i = 0; i < n; i++) {
+            vector<int> new_dp(m + 1);
             for(int j = 0; j < m; j++) {
                 if(i == 0 && j == 0) {
-                    continue;
+                    new_dp[j] = grid[i][j] == 0;
                 }
                 else if(i == 0) {
                     if(grid[i][j] == 0) {
-                        dp[i][j] += dp[i][j - 1];
+                        new_dp[j] += new_dp[j - 1];
                     }
                 }
                 else if(j == 0) {
                     if(grid[i][j] == 0) {
-                        dp[i][j] += dp[i - 1][j];
+                        new_dp[j] += dp[j];
                     }
                 }
                 else {
                     if(grid[i][j] == 0) {
-                        dp[i][j] += dp[i][j - 1];
-                        dp[i][j] += dp[i - 1][j];
+                        new_dp[j] += new_dp[j - 1];
+                        new_dp[j] += dp[j];
                     }
                 }
             }
+            dp = new_dp;
         }
-        return dp[n - 1][m - 1];
+        return dp[m - 1];
     }
 };
