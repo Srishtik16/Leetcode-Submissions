457aced30bed0d27f214d2e13b783f4d4a11a045
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-06-09 19:37:00 +0530

    Time: 300 ms (5.03%), Space: 20.7 MB (6.22%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& a) {
+        int n = a.size();
+        vector<int> prev(n), next(n);
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            while(!s.empty() && a[s.top()] >= a[i]) {
+                s.pop();
+            }
+            if(s.empty()) {
+                prev[i] = -1;
+            }
+            else {
+                prev[i] = s.top();
+            }
+            s.push(i);
+        }
+        while(!s.empty()) {
+            s.pop();
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            while(!s.empty() && a[s.top()] >= a[i]) {
+                s.pop();
+            }
+            if(s.empty()) {
+                next[i] = n;
+            }
+            else {
+                next[i] = s.top();
+            }
+            s.push(i);
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, (abs(prev[i] - next[i]) - 1) * a[i]);
+        }
+        return ans;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> mat(n, vector<int>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat[i][j] = matrix[i][j] == '0' ? 0 : 1;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                mat[j][i] += mat[j - 1][i];
+                if(matrix[j][i] == '0') {
+                    mat[j][i] = 0;
+                }
+            }
+        }
+        int ans = 0;
+        vector<vector<int>> aux(m, vector<int>(n));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                aux[i][j] = mat[j][i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == '0') {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        for(auto x: mat) {
+            for(auto y: x) {
+                cout << y << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        for(auto x: mat) {
+            ans = max(ans, largestRectangleArea(x));
+            cout << largestRectangleArea(x) << endl;
+        }
+        return ans;
+    }
+};
