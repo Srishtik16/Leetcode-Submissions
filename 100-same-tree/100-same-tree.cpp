d889c5906dc512d894c2feaa5dc47d6df1c94c91
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-06-11 00:19:55 +0530

    Time: 7 ms (18.04%), Space: 10.5 MB (7.13%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -20,32 +20,10 @@ public:
         preOrder(root -> right, ans);
         ans.push_back(root -> val);
     }
-    void inOrder(TreeNode* root, vector<int> &ans) {
-        if(root == NULL) {
-            ans.push_back(INT_MIN);
-            return;
-        }
-        inOrder(root -> left, ans);
-        inOrder(root -> right, ans);
-        ans.push_back(root -> val);
-    }
-    void postOrder(TreeNode* root, vector<int> &ans) {
-        if(root == NULL) {
-            ans.push_back(INT_MIN);
-            return;
-        }
-        postOrder(root -> left, ans);
-        postOrder(root -> right, ans);
-        ans.push_back(root -> val);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> p1, p2, p3, p4, p5, p6;
+        vector<int> p1, p4;
         preOrder(p, p1);
-        inOrder(p, p2);
-        postOrder(p, p3);
         preOrder(q, p4);
-        inOrder(q, p5);
-        postOrder(q, p6);
-        return p1 == p4 && p2 == p5 && p3 == p6;
+        return p1 == p4;
     }
 };
