54bdc992a1a986070d6a967db1fdfc67caab4885
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-06-21 02:04:18 +0530

    Time: 61 ms (79.41%), Space: 73.9 MB (23.53%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,93 @@
+struct Node {
+    Node* links[27]; // Links which form the trie
+    bool isEnd = false; // Checks if this is end of current word
+    // Checks if current key is present in the TrieNode
+    bool containsKey(char c) {
+        return links[c - 'a'] != NULL;
+    }
+    // Inserts key in the current TrieNode
+    void put(char c, Node* node) {
+        links[c - 'a'] = node;
+    }
+    // Gets Node pointed to by current link
+    Node* get(char c) {
+        return links[c - 'a'];
+    }
+    // Decides the end of a word
+    void setEnd() {
+        isEnd = true;
+    }
+    // Checks if a word has ended or not
+    bool isEnding() {
+        return isEnd;
+    }
+};
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        // Creates new trienode
+        root = new Node();
+    }
+    // TC: O(length of word)
+    void insert(string word) {
+        // Dummy node pointing to root.
+        Node* node = root;
+        for(char c: word) {
+            // If key is not present in Trie, insert that key into the trie node.
+            if(!node -> containsKey(c)) {
+                node -> put(c, new Node());
+            }
+            // Moves to the reference Trie
+            node = node -> get(c);
+        }
+        // Mark end of current word
+        node -> setEnd();
+    }
+    // TC: O(length of word)
+    bool search(string word) {
+        Node* node = root;
+        for(char c: word) {
+            // If key is not present, word doesnt exist
+            if(!node -> containsKey(c)) {
+                return false;
+            }
+            // Go to next link address
+            node = node -> get(c);
+        }
+        // Check if word has ended
+        return node -> isEnding();
+    }
+    // TC: O(length of word)
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(char c: prefix) {
+            // If key is not present, word doesn't exist
+            if(!node -> containsKey(c)) {
+                return false;
+            }
+            // Go to next link address
+            node = node -> get(c);
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        Trie* trie = new Trie();
+        sort(words.begin(), words.end(), [&](const auto &x, const auto &y) {
+           return x.size() > y.size(); 
+        });
+        int ans = 0;
+        for(auto &x: words) {
+            reverse(x.begin(), x.end());
+            if(!trie -> startsWith(x)) {
+                ans += x.size() + 1;
+            }
+            trie -> insert(x);
+        }
+        return ans;
+    }
+};
