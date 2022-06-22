d25ca6b2b2b92e861fee44532c05a77868fca73b
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-06-22 09:36:21 +0530

    Time: 99 ms (44.52%), Space: 24.5 MB (5.05%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int largestSquareArea(vector<int>& a) {
+        int n = a.size();
+        vector<int> prev(n), next(n);
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            while(!s.empty() && a[s.top()] >= a[i]) {
+                s.pop();
+            }
+            if(s.empty()) {
+                prev[i] = -1;
+            }
+            else {
+                prev[i] = s.top();
+            }
+            s.push(i);
+        }
+        while(!s.empty()) {
+            s.pop();
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            while(!s.empty() && a[s.top()] >= a[i]) {
+                s.pop();
+            }
+            if(s.empty()) {
+                next[i] = n;
+            }
+            else {
+                next[i] = s.top();
+            }
+            s.push(i);
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            int side = min((abs(prev[i] - next[i]) - 1), a[i]);
+            ans = max(ans, side * side);
+        }
+        return ans;
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> mat(n, vector<int>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mat[i][j] = matrix[i][j] == '0' ? 0 : 1;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                mat[j][i] += mat[j - 1][i];
+                if(matrix[j][i] == '0') {
+                    mat[j][i] = 0;
+                }
+            }
+        }
+        int ans = 0;
+        for(auto x: mat) {
+            ans = max(ans, largestSquareArea(x));
+        }
+        return ans;
+    }
+};
