398e68079188f271665b0a1ae8aeef75f2fbabf9
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-06-30 01:25:20 +0530

    Time: 7 ms (71.45%), Space: 11.2 MB (5.43%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        multiset<int> ms1(nums1.begin(), nums1.end()), ms2(nums2.begin(), nums2.end());
+        vector<int> ans;
+        for(auto x: ms1) {
+            if(ms2.find(x) != ms2.end()) {
+                ans.push_back(x);
+                ms2.erase(ms2.find(x));
+            }
+        }
+        return ans;
+    }
+};
