bf276d8cc45ffad4f8809c1dad413b276f2cf5ec
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-02 12:14:35 +0530

    Time: 2712 ms (5.13%), Space: 319.1 MB (9.22%) - LeetHub

---
+++ 898-bitwise-ors-of-subarrays/898-bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& A) {
+        int n = A.size();
+        // prev -> pref[i - 1]
+        // curr -> pref[i]
+        unordered_set<int> prev, ans;
+        for(int i = 0; i < n; i++) {
+            unordered_set<int> curr;
+            curr.insert(A[i]);
+            ans.insert(A[i]);
+            for(auto x: prev) {
+                curr.insert(x | A[i]);
+                ans.insert(x | A[i]);
+            }
+            prev = curr;
+        }
+        return ans.size();
+    }
+};
+
+// I want to count number of distinct values for subarrays ending at i
+// pref[i - 1] = OR of 1st i - 1 elements
+// I am standing at i
+// pref[i - 1] | a[i]
+// pref[i] --> Atmost 30 different values
+// I want to compute ending at i
+// Answer = sum of all answer at i
+// If I am i
+// a[i], a[i] | a[i - 1], a[i]
