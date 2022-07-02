97cd544219cceb6068c838be3ce8350924d695d3
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-02 21:44:53 +0530

    Time: 23 ms (47.13%), Space: 13.8 MB (31.22%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -16,34 +16,17 @@ public:
             return min(x, y) + 1440 - (max(x, y));
         };
         sort(timePoints.begin(), timePoints.end());
-        for(auto x: timePoints) {
-            cout << x << " ";
-        }
-        cout << endl;
         vector<int> time1, time2;
         for(auto x: timePoints) {
             time1.push_back(convert(x));
         }
-        for(auto x: time1) {
-            cout << x << " ";
-        }
-        cout << endl;
-        for(auto x: time2) {
-            cout << x << " ";
-        }
-        cout << endl;
-        // sort(time1.begin(), time1.end());
-        // sort(time2.begin(), time2.end());
         int ans = INT_MAX;
         for(int i = 1; i < time1.size(); i++) {
-            // cout << time1[i] << " " << time2[i] << endl;
             ans = min(ans, clockWise(time1[i], time1[i - 1]));
             ans = min(ans, antiClockWise(time1[i], time1[i - 1]));
-            // ans = min(ans, abs(time2[i] - time2[i - 1]));
         }
         ans = min(ans, clockWise(time1[0], time1.back()));
         ans = min(ans, antiClockWise(time1[0], time1.back()));
-        cout << antiClockWise(time1[0], time1.back()) << endl;
         return ans;
     }
 };
