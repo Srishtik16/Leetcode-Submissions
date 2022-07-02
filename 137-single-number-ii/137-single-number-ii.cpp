a55f6a7a71630e49f16681a951ceffdcdc7ccbb7
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-02 13:59:51 +0530

    Time: 18 ms (23.86%), Space: 9.6 MB (56.35%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,14 +2,22 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int ones = 0, twos = 0;
-        for(auto x: nums) {
-            twos |= (ones & x);
-            ones ^= x;
-            int cancel = ~(ones & twos);
-            ones &= cancel;
-            twos &= cancel;
+        vector<long long> hash(32);
+        for(int i = 0; i <= 31; i++) {
+            for(int j = 0; j < n; j++) {
+                if(nums[j] & (1LL << i)) {
+                    hash[i]++;
+                }
+            }
         }
-        return ones;
+        long long ans = 0;
+        // 31st bit 1 to signify its negative
+        for(int i = 31; i >= 0; i--) {
+            if(hash[i] % 3 == 0) {
+                continue;
+            }
+            ans |= (1LL << i);
+        }
+        return ans;
     }
 };
