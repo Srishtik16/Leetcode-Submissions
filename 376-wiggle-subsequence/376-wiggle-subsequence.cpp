b65d50e67a27a4442cd0b2ae85cbe86dfc89ed6e
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-03 10:34:37 +0530

    Time: 4 ms (56.21%), Space: 7.6 MB (11.24%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2, 1));
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = dp[0][1] = 1;
         for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[i] - nums[j] > 0) {
-                    dp[i][0] = max(dp[i][0], dp[j][1] + 1);
-                }
-                else if(nums[i] - nums[j] < 0) {
-                    dp[i][1] = max(dp[i][1], dp[j][0] + 1);
-                }
-                else {
-                    continue;
-                }
+            if(nums[i] > nums[i - 1]) {
+                dp[i][0] = dp[i - 1][1] + 1;
+                dp[i][1] = dp[i - 1][1];
+            }
+            else if(nums[i] < nums[i - 1]) {
+                dp[i][1] = dp[i - 1][0] + 1;
+                dp[i][0] = dp[i - 1][0];
+            }
+            else {
+                dp[i][0] = dp[i - 1][0];
+                dp[i][1] = dp[i - 1][1];
             }
         }
         int ans = 0;
