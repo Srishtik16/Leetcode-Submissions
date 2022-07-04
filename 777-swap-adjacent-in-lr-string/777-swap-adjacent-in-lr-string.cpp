acc61973ec71b667a03b3abb4a1ae0fa605b2517
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-04 13:42:20 +0530

    Time: 18 ms (15.32%), Space: 8.9 MB (10.36%) - LeetHub

---
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool canTransform(string start, string end) {
+        string s = "", t = "";
+        vector<vector<int>> v(4);
+        for(char c: start) {
+            if(c != 'X') {
+                s += c;
+            }
+        }
+        for(char c: end) {
+            if(c != 'X') {
+                t += c;
+            }
+        }
+        if(s != t) {
+            return false;
+        }
+        for(int i = 0; i < (int)start.size(); i++) {
+            if(start[i] == 'X') {
+                continue;
+            }
+            if(start[i] == 'L') {
+                v[0].push_back(i);
+            }
+            else {
+                v[1].push_back(i);
+            }
+        } 
+        for(int i = 0; i < (int)end.size(); i++) {
+            if(end[i] == 'X') {
+                continue;
+            }
+            if(end[i] == 'L') {
+                v[2].push_back(i);
+            }
+            else {
+                v[3].push_back(i);
+            }
+        } 
+        for(int i = 0; i < v[0].size(); i++) {
+            if(v[0][i] < v[2][i]) {
+                return false;
+            }
+        }
+        for(int i = 0; i < v[1].size(); i++) {
+            if(v[1][i] > v[3][i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
