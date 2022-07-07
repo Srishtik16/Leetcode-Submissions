cbb609dee0c57ba4cfd681d48953fc2bd73d700a
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-07 09:34:10 +0530

    Time: 13 ms (40.06%), Space: 6.2 MB (96.38%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -9,19 +9,19 @@ public:
         }
         // dp[i][j] = Is the string formed by the prefix i of s1 and prefix j of s2
         // an interleaving of the prefix i + j of s3?
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
-        for(int i = 1; i <= n; i++) {
-            dp[i][0] = dp[i - 1][0] & (s1[i - 1] == s3[i - 1]);
-        }
+        vector<bool> dp(m + 1, false);
+        dp[0] = true;
         for(int i = 1; i <= m; i++) {
-            dp[0][i] = dp[0][i - 1] & (s2[i - 1] == s3[i - 1]);
+            dp[i] = dp[i - 1] & (s2[i - 1] == s3[i - 1]);
         }
         for(int i = 1; i <= n; i++) {
+            vector<bool> new_dp(m + 1, false);
+            new_dp[0] = dp[0] & (s1[i - 1] == s3[i - 1]);
             for(int j = 1; j <= m; j++) {
-                dp[i][j] = dp[i][j] | (dp[i - 1][j] & (s1[i - 1] == s3[i + j - 1])) | (dp[i][j - 1] & (s2[j - 1] == s3[i + j - 1]));
+                new_dp[j] = new_dp[j] | (dp[j] & (s1[i - 1] == s3[i + j - 1])) | (new_dp[j - 1] & (s2[j - 1] == s3[i + j - 1]));
             }
+            dp = new_dp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
