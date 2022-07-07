4d4d35c36910a5c97c12293d80d09ecda8f0a8ac
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-07 21:19:19 +0530

    Time: 39 ms (24.62%), Space: 13.3 MB (36.86%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        vector<bool> dp(n + 1, false);
-        dp[0] = true;
-        for(int i = 1; i <= n; i++) {
-            for(int j = i - 1; j >= 0; j--) {
-                if(dp[j]) {
-                    string st = s.substr(j, i - j);
-                    if(find(wordDict.begin(), wordDict.end(), st) != wordDict.end()) {
-                        dp[i] = true;
-                        break;
-                    }
+    vector<int> dp;
+    bool recurse(int index, string &s, vector<string> &dict) {
+        if(index >= s.size()) {
+            return true;
+        }
+        if(dp[index] != -1) {
+            return dp[index] == 0 ? false : true;
+        }
+        for(int i = index + 1; i <= s.size(); i++) {
+            string st = s.substr(index, i - index);
+            if(find(dict.begin(), dict.end(), st) != dict.end()) {
+                if(recurse(i, s, dict)) {
+                    return true;
+                }
+                else {
+                    dp[i] = 0;
                 }
             }
         }
-        return dp.back();
+        dp[index] = 0;
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        dp.assign(n + 1, -1);
+        return recurse(0, s, wordDict);
     }
 };
