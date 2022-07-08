a819c9fea5827b87a9dcd420abb4b71842390de6
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-08 08:54:58 +0530

    Time: 257 ms (37.89%), Space: 69.9 MB (70.89%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,54 +1,28 @@
-class fenwick_tree{
-private:
-    vector<long long> BIT;
-public:
-    fenwick_tree(int n){
-        BIT.resize(n + 1, 0);
-    }
-    long long query(int i){
-        long long res = 0;
-        while(i >= 0){
-            res += BIT[i];
-            i = (i & (i + 1)) - 1;
-        }
-        return res;
-    }
-    void update(int i, int val){
-        while(i < BIT.size()){
-            BIT[i] += val;
-            i = (i | (i + 1));
-        }
-    }
-};
 class ProductOfNumbers {
 public:
-    vector<fenwick_tree> ft;
-    int timeStamp;
+    vector<int> prefixProduct;
+    int product;
     ProductOfNumbers() {
-        for(int i = 0; i <= 100; i++) {
-            fenwick_tree ft1(40005);
-            ft.push_back(ft1);
-        }
-        timeStamp = 0;
+        prefixProduct.push_back(1);
+        product = 1;
     }
     
     void add(int num) {
-        ft[num].update(timeStamp, 1);
-        timeStamp++;
+        if(num == 0) {
+            prefixProduct.clear();
+            product = 1;
+            prefixProduct.push_back(product);
+            return;
+        }
+        product *= num;
+        prefixProduct.push_back(product);
     }
     
     int getProduct(int k) {
-        int product = 1;
-        for(int i = 0; i <= 100; i++) {
-            if((ft[i].query(timeStamp - 1) - ft[i].query(timeStamp - k - 1)) == 0) {
-                continue;
-            }
-            if(product == 0) {
-                break;
-            }
-            product *= powl(i, (ft[i].query(timeStamp - 1) - ft[i].query(timeStamp - k - 1)));
+        if(prefixProduct.size() <= k) {
+            return 0;
         }
-        return product;
+        return prefixProduct.back() / prefixProduct[prefixProduct.size() - k - 1];
     }
 };
 
