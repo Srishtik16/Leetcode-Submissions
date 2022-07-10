f827f879d82aec8870e8911712815a5b1b3e202f
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-10 21:05:08 +0530

    Time: 13 ms (24.13%), Space: 7.7 MB (69.70%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.cpp
+++ 920-number-of-music-playlists/920-number-of-music-playlists.cpp
@@ -180,13 +180,15 @@ public:
         // dp[0][j] = 0
         // dp[i][0] = 0
         // dp[i][j] = dp[i - 1][j] * max(j - k, 0) + dp[i - 1][j - 1] * (n - j + 1);
-        vector<vector<Mint>> dp(goal + 1, vector<Mint> (n + 1));
-        dp[0][0] = 1;
+        vector<Mint> dp(n + 1);
+        dp[0] = 1;
         for(int i = 1; i <= goal; i++) {
+            vector<Mint> new_dp(n + 1);
             for(int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i - 1][j] * max(0, j - k) + dp[i - 1][j - 1] * (n - j + 1);
+                new_dp[j] = dp[j] * max(0, j - k) + dp[j - 1] * (n - j + 1);
             }
+            dp = new_dp;
         }
-        return (int)dp[goal][n];
+        return (int)dp[n];
     }
 };
