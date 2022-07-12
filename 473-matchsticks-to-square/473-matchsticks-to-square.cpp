77d19ed88a61ddb4de866b092f7ed51ada9eed49
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-12 09:10:08 +0530

    Time: 608 ms (37.12%), Space: 10 MB (66.64%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> vis;
+    bool recurse(int index, int k, int sum, int target, vector<int> &nums, int n) {
+        if(k == 1) {
+            return true;
+        }
+        if(index >= n) {
+            return false;
+        }
+        if(sum > target) {
+            return false;
+        }
+        if(sum == target) {
+            return recurse(0, k - 1, 0, target, nums, n);
+        }
+        for(int i = index; i < n; i++) {
+            if(vis[i]) {
+                continue;
+            }
+            vis[i] = true;
+            if(recurse(i + 1, k, sum + nums[i], target, nums, n)) {
+                return true;
+            }
+            vis[i] = false;
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size();
+        vis.assign(n + 1, false);
+        int k = 4;
+        int target = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        if(target % k != 0) {
+            return false;
+        }
+        target /= k;
+        return recurse(0, k, 0, target, matchsticks, n);
+    }
+};
