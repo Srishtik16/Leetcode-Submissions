707e9307423154db2552742636a037bd29557551
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-14 10:11:52 +0530

    Time: 192 ms (22.30%), Space: 24.1 MB (20.79%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,28 +1,38 @@
-typedef pair<int, pair<int, int>> pii;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> vec(n);
-        for(auto x: flights) {
-            vec[x[0]].push_back({x[1], x[2]});
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto &edge: flights) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
         }
-        priority_queue<pii, vector<pii>, greater<pii>> pq;
-        vector<int> minStops(n, INT_MAX);
-        pq.push({0, {src, 0}});
+        vector<vector<int>> dp(n + 4, vector<int>(k + 4, INT_MAX));
+        set<vector<int>> pq;
+        // {current distance, node, number of stops}
+        pq.insert({0, src, 0});
+        dp[src][0] = 0;
         while(!pq.empty()) {
-            auto f = pq.top();
-            pq.pop();
-            if(f.second.first == dst) {
-                return f.first;
-            }
-            if(f.second.second > k || minStops[f.second.first] <= f.second.second) {
-                continue;
-            }
-            minStops[f.second.first] = min(minStops[f.second.first], f.second.second);
-            for(auto x: vec[f.second.first]) {
-                pq.push({f.first + x.second, {x.first, f.second.second + 1}});
+            auto u = *pq.begin();
+            pq.erase(pq.begin());
+            for(auto x: adj[u[1]]) {
+                if(u[2] + 1 <= k + 1 && dp[x.first][u[2] + 1] == INT_MAX) {
+                    dp[x.first][u[2] + 1] = min(dp[u[1]][u[2]] + x.second, dp[x.first][u[2] + 1]);
+                    vector<int> v = {dp[x.first][u[2] + 1], x.first, u[2] + 1};
+                    pq.insert(v);
+                }
+                else if(u[2] + 1 <= k + 1 && dp[x.first][u[2] + 1] > dp[u[1]][u[2]] + x.second) {
+                    dp[x.first][u[2] + 1] = min(dp[u[1]][u[2]] + x.second, dp[x.first][u[2] + 1]);
+                    vector<int> v = {dp[x.first][u[2] + 1], x.first, u[2] + 1};
+                    pq.insert(v);
+                }
             }
         }
-        return -1;
+        int ans = INT_MAX;
+        for(int i = 0; i <= k + 1; i++) {
+            ans = min(ans, dp[dst][i]);
+        }
+        if(ans == INT_MAX) {
+            ans = -1;
+        }
+        return ans;
     }
 };
