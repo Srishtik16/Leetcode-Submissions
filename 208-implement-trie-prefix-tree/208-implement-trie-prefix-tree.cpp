4743e7135cd62f26ebc680db21d17a18bfa34d9b
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-18 16:22:23 +0530

    Time: 86 ms (67.67%), Space: 45.1 MB (51.20%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,73 +1,58 @@
-struct Node {
-    Node* links[26]; // Links which form the trie
-    bool isEnd = false; // Checks if this is end of current word
-    // Checks if current key is present in the TrieNode
+class Node {
+public:
+    Node* links[26];
+    bool isEnding;
     bool containsKey(char c) {
         return links[c - 'a'] != NULL;
     }
-    // Inserts key in the current TrieNode
     void put(char c, Node* node) {
         links[c - 'a'] = node;
     }
-    // Gets Node pointed to by current link
     Node* get(char c) {
         return links[c - 'a'];
     }
-    // Decides the end of a word
     void setEnd() {
-        isEnd = true;
+        isEnding = true;
     }
-    // Checks if a word has ended or not
-    bool isEnding() {
-        return isEnd;
+    bool isEnd() {
+        return isEnding;
     }
 };
 class Trie {
-private:
     Node* root;
 public:
     Trie() {
-        // Creates new trienode
         root = new Node();
     }
-    // TC: O(length of word)
+    
     void insert(string word) {
-        // Dummy node pointing to root.
         Node* node = root;
         for(char c: word) {
-            // If key is not present in Trie, insert that key into the trie node.
             if(!node -> containsKey(c)) {
                 node -> put(c, new Node());
             }
-            // Moves to the reference Trie
             node = node -> get(c);
         }
-        // Mark end of current word
         node -> setEnd();
     }
-    // TC: O(length of word)
+    
     bool search(string word) {
         Node* node = root;
         for(char c: word) {
-            // If key is not present, word doesnt exist
             if(!node -> containsKey(c)) {
                 return false;
             }
-            // Go to next link address
             node = node -> get(c);
         }
-        // Check if word has ended
-        return node -> isEnding();
+        return node -> isEnd();
     }
-    // TC: O(length of word)
+    
     bool startsWith(string prefix) {
         Node* node = root;
         for(char c: prefix) {
-            // If key is not present, word doesn't exist
             if(!node -> containsKey(c)) {
                 return false;
             }
-            // Go to next link address
             node = node -> get(c);
         }
         return true;
