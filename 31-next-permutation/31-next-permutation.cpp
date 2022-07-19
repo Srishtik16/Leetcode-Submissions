df70cf37fc33db62fafc31be49e271f13911e329
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-07-19 10:04:40 +0530

    Time: 14 ms (9.96%), Space: 12.1 MB (74.62%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-        reverse(a.begin(), a.end());
-        if(is_sorted(a.begin(), a.end())) {
-            return;
-        }
-        reverse(a.begin(), a.end());
         int n = a.size();
-        int ind1 = 0;
-        for(int i = n - 1; i >= 1; i--) {
-            if(a[i] > a[i - 1]) {
-                ind1 = i - 1;
-                break;
-            }
+        bool isSorted = true;
+        for(int i = 0; i < n - 1; i++) {
+            isSorted &= a[i] >= a[i + 1];
         }
-        int ind2 = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            if(a[ind1] < a[i]) {
-                ind2 = i;
-                break;
-            }
-        }
-        if(ind2 < ind1) {
+        if(isSorted) {
             reverse(a.begin(), a.end());
             return;
         }
-        swap(a[ind1], a[ind2]);
-        reverse(a.begin() + ind1 + 1, a.end());
-        return;
+        int indexWhereDecrease = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            if(a[i] < a[i + 1]) {
+                indexWhereDecrease = i;
+                break;
+            }
+        }
+        int indexToBeSwapped = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(a[indexWhereDecrease] < a[i]) {
+                indexToBeSwapped = i;
+                break;
+            }
+        }
+        if(indexToBeSwapped < indexWhereDecrease) {
+            reverse(a.begin(), a.end());
+            return;
+        }
+        swap(a[indexWhereDecrease], a[indexToBeSwapped]);
+        int j = n - 1;
+        for(int i = indexWhereDecrease + 1; i < n; i++) {
+            if(i >= j) {
+                break;
+            }
+            swap(a[i], a[j]);
+            j--;
+        }
     }
 };
