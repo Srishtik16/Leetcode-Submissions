9fd4e3f8f12148982640874210d0ce7214ad6ea5
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-09-21 09:31:38 +0530

    Time: 284 ms (12.30%), Space: 49.9 MB (16.27%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int sum = 0;
+        for(auto x: nums) {
+            if(x % 2 == 0) {
+                sum += x;
+            }
+        }
+        vector<int> ans;
+        for(auto x: queries) {
+            int index = x[1];
+            int val = x[0];
+            if(nums[index] % 2 == 0) {
+                sum -= nums[index];
+            }
+            nums[index] += val;
+            if(nums[index] % 2 == 0) {
+                sum += nums[index];
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
