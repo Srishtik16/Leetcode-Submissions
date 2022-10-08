1abb16c76c464bed41f6a07bb5ea3c78b407081f
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-10-08 11:30:08 +0530

    Time: 597 ms (27.30%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -2,51 +2,20 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int wholeDiff = INT_MAX, wholeSum = 0;
-        vector<int> ans;
-        for(int i = 0; i < n; i++) {
+        int n = nums.size(), ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n-2; ++i) {
             int l = i + 1, r = n - 1;
-            int sum = target - nums[i];
-            int minDiff = INT_MAX, minSum = 0;
-            vector<int> res;
-            while(l < r) {
-                if(nums[l] + nums[r] == sum) {
-                    int diff = abs(nums[l] + nums[r] - sum);
-                    if(diff < minDiff) {
-                        minDiff = diff;
-                        minSum = nums[l] + nums[r];
-                        res = {nums[l], nums[r]};
-                    }
-                    break;
-                }
-                else if(nums[l] + nums[r] > sum) {
-                    int diff = abs(nums[l] + nums[r] - sum);
-                    if(diff < minDiff) {
-                        minDiff = diff;
-                        minSum = nums[l] + nums[r];
-                        res = {nums[l], nums[r]};
-                    }
-                    r--;
-                }
-                else {
-                    int diff = abs(nums[l] + nums[r] - sum);
-                    if(diff < minDiff) {
-                        minDiff = diff;
-                        minSum = nums[l] + nums[r];
-                        res = {nums[l], nums[r]};
-                    }
-                    l++;
-                }
-            }
-            int ssum = minSum + nums[i];
-            if(abs(ssum - target) < wholeDiff) {
-                wholeDiff = abs(ssum - target);
-                wholeSum = ssum;
-                ans = res;
-                ans.push_back(nums[i]);
+            while (l < r) {
+                int sum3 = nums[i] + nums[l] + nums[r];
+                if (abs(ans - target) > abs(sum3 - target))
+                    ans = sum3;
+                if (sum3 == target) break;
+                if (sum3 > target)
+                    --r;
+                else
+                    ++l;
             }
         }
-        return ans[0] + ans[1] + ans[2];
+        return ans;
     }
 };
