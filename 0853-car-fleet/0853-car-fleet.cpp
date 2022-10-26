7d931af3747c19bca93716894662fe8bed9ada9f
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-10-27 00:27:54 +0530

    Time: 490 ms (38.33%), Space: 79.8 MB (50.48%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -7,15 +7,6 @@ public:
             a[i] = {position[i], speed[i]};
         }
         sort(a.begin(), a.end());
-        auto compare = [&](int i, int j) {
-            int p1 = a[i].first;
-            int p2 = a[j].first;
-            int s1 = a[i].second;
-            int s2 = a[j].second;
-            int t1 = (target - p1) * s2;
-            int t2 = (target - p2) * s1;
-            return t1 <= t2;
-        };
         vector<long double> time(n);
         for(int i = 0; i < n; i++) {
             time[i] = 1.0 * (target - a[i].first) / a[i].second;
