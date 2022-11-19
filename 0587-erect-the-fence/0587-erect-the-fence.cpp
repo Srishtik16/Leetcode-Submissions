67ba9394e4aa2c07957b97efc8ac4200890cb505
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-11-19 14:43:37 +0530

    Time: 128 ms (56.80%), Space: 23.5 MB (30.40%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -0,0 +1,59 @@
+struct pt {
+    double x, y;
+};
+
+int orientation(pt a, pt b, pt c) {
+    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
+    if (v < 0) return -1; // clockwise
+    if (v > 0) return +1; // counter-clockwise
+    return 0;
+}
+
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
+}
+bool collinear(pt a, pt b, pt c) { return orientation(a, b, c) == 0; }
+
+void convex_hull(vector<pt>& a, bool include_collinear = true) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
+                < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+        return o < 0;
+    });
+    if (include_collinear) {
+        int i = (int)a.size()-1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin()+i+1, a.end());
+    }
+
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        st.push_back(a[i]);
+    }
+
+    a = st;
+}
+class Solution {
+public:
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        vector<pt> points;
+        for(auto x: trees) {
+            points.push_back({(double)x[0], (double)x[1]});
+        }
+        convex_hull(points);
+        vector<vector<int>> ans;
+        for(auto x: points) {
+            vector<int> p = {(int)x.x, (int)x.y};
+            ans.push_back(p);
+        }
+        return ans;
+    }
+};
