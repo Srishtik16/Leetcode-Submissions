28424b9151db42a97569b5da7fddd2bb5e8f4ba6
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2022-12-01 19:30:10 +0530

    Time: 4 ms (45.55%), Space: 7.4 MB (80.29%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int minStartValue(vector<int>& nums) {
-        auto check = [&](int x) {
-            int sum = x;
-            for(auto x: nums) {
-                sum += x;
-                if(sum < 1) {
-                    return false;
-                }
+    bool check(vector<int> &nums, int x) {
+        int sum = x;
+        for(auto x: nums) {
+            sum += x;
+            if(sum < 1) {
+                return false;
             }
-            return true;
-        };
+        }
+        return true;
+    }
+    int minStartValue(vector<int>& nums) {
         int l = 1, r = 1e9, ans = 1;
         while(l <= r) {
             int mid = (l + r) / 2;
-            if(check(mid)) {
+            if(check(nums, mid)) {
                 ans = mid;
                 r = mid - 1;
             }
