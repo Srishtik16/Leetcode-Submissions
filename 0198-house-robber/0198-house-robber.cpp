3d08ad88e04e6a077ef263d42d6938dbb9aea989
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-14 11:10:30 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (51.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,15 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         // dp[i][0 / 1] = maximum cost from 1st i houses when not taking/taking from ith house
-        vector<vector<int>> dp(n, vector<int>(2, 0));
-        dp[0][0] = 0;
-        dp[0][1] = nums[0];
+        vector<int> dp(2, 0);
+        dp[0] = 0;
+        dp[1] = nums[0];
         for(int i = 1; i < n; i++) {
-            dp[i][0] = max(dp[i - 1][1], dp[i - 1][0]);
-            dp[i][1] = dp[i - 1][0] + nums[i];
+            vector<int> new_dp(2, 0);
+            new_dp[0] = max(dp[1], dp[0]);
+            new_dp[1] = dp[0] + nums[i];
+            dp.swap(new_dp);
         }
-        return max(dp[n - 1][0], dp[n - 1][1]);
+        return max(dp[0], dp[1]);
     }
 };
