30b3ecfbbd2701381b1d16a1012ac7a6013a12cb
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-18 00:48:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.88%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n == 1) {
+            return 0;
+        }
+        else if(k % 2 == 0) {
+            return kthGrammar(n - 1, k / 2) ^ 1;
+        }
+        else {
+            return kthGrammar(n - 1, k / 2 + 1);
+        }
+    }
+};
