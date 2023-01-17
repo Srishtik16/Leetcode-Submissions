3c47d8f7a41a6ea0aece15f532df2412b5b4bbc4
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-18 00:13:57 +0530

    Time: 46 ms (26.67%), Space: 8.1 MB (19.42%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
@@ -6,17 +6,25 @@ public:
         for(int i = 0; i < n; i++) {
             pref[i + 1] = pref[i] + nums[i];
         }
-        vector<double> dp(n);
-        for(int i = 0; i < n; i++) {
-            dp[i] = (pref.back() - pref[i]) / (n - i);
+        // dp[i][j] = answer for 1st i elements with atmost j partitions
+        // Base Case
+        // dp[0][0] = 0, dp[i][0] = pref[i], dp[0][j] = 0
+        // Transistions
+        // dp[i][j] = max(dp[k][j - 1] + cost(k, i)) over all k from 0 to i - 1
+        vector<vector<double>> dp(n + 1, vector<double>(K + 1, 0));
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = pref[i] / i;
         }
-        for(int k = 0; k < K - 1; k++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = i + 1; j < n; j++) {
-                    dp[i] = max(dp[i], (pref[j] - pref[i]) / (j - i) + dp[j]);
+        auto cost = [&](int l, int r) {
+            return (pref[r] - pref[l]) / (r - l);
+        };
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                for(int k = 1; k < K; k++) {
+                    dp[i][k] = max(dp[i][k], dp[j][k - 1] + cost(j, i));
                 }
             }
         }
-        return dp.front();
+        return dp[n][K - 1];
     }
 };
