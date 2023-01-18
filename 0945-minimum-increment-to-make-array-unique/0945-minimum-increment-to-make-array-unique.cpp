897c4aa63874d0a1b33c8e873b8e0844f7f69245
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-19 00:38:07 +0530

    Time: 788 ms (7.56%), Space: 156.6 MB (6.24%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,30 @@
+const int MaxN = 1e6 + 5;
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        multiset<int> ms(nums.begin(), nums.end());
+        set<int> uniques(nums.begin(), nums.end());
+        for(auto x: uniques) {
+            ms.erase(ms.find(x));
+        }
+        vector<bool> vis(MaxN, false);
+        for(auto x: nums) {
+            vis[x] = true;
+        }
+        int moves = 0;
+        for(int i = 1; i < MaxN; i++) {
+            if(ms.empty()) {
+                break;
+            }
+            if(!vis[i]) {
+                if(*ms.begin() > i) {
+                    continue;
+                }
+                moves += abs(i - *ms.begin());
+                ms.erase(ms.begin());
+                vis[i] = true;
+            }
+        }
+        return moves;
+    }
+};
