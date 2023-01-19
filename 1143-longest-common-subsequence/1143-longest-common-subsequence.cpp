18214c831806d9a9b76fbca3b163abca9e0865fd
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-19 22:07:28 +0530

    Time: 475 ms (5.02%), Space: 17.3 MB (12.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,85 @@
+class SegmentTree {
+public:
+    vector<int> st;
+    int n;
+    SegmentTree(int _n) {
+        n = _n;
+        st.assign(4 * n, 0);
+    }
+    int merge(int x, int y) {
+        return max(x, y);
+    }
+    void build(int node, int start, int end, vector<int> &a) {
+        if(start == end) {
+            st[node] = a[start];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(2 * node + 1, start, mid, a);
+        build(2 * node + 2, mid + 1, end, a);
+        st[node] = merge(st[2 * node + 1], st[2 * node + 2]);
+    }
+    int query(int node, int start, int end, int l, int r) {
+        if(start >= l && end <= r) {
+            return st[node];
+        }
+        if(start > r || end < l) {
+            return INT_MIN;
+        }
+        int mid = (start + end) / 2;
+        return merge(query(2 * node + 1, start, mid, l, r), query(2 * node + 2, mid + 1, end, l, r));
+    }
+    void update(int node, int start, int end, int index, int val) {
+        if(start == end && start == index) {
+            st[node] = val;
+            return;
+        }
+        if(end < index || start > index) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        update(2 * node + 1, start, mid, index, val);
+        update(2 * node + 2, mid + 1, end, index, val);
+        st[node] = merge(st[2 * node + 1], st[2 * node + 2]);
+    }
+    int query(int l, int r) {
+        return query(0, 0, n - 1, l, r);
+    }
+    void update(int index, int val) {
+        update(0, 0, n - 1, index, val);
+    }
+};
 class Solution {
 public:
     int longestCommonSubsequence(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        // dp[i][j] = LCS of s[0, i] and t[0, j]
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        map<char, vector<int>> adj;
+        for(int i = 0; i < m; i++) {
+            adj[t[i]].push_back(i);
+        }
+        for(auto &x: adj) {
+            sort(x.second.rbegin(), x.second.rend());
+        }
+        vector<int> nums;
+        for(char c: s) {
+            for(auto x: adj[c]) {
+                nums.push_back(x + 1);
             }
         }
-        return dp[n][m];
+        if(nums.empty()) {
+            return 0;
+        }
+        int mx = *max_element(nums.begin(), nums.end());
+        SegmentTree st(mx + 1);
+        int ans = 1;
+        n = nums.size();
+        for(int i = 0; i < n; i++) {
+            int l = 0;
+            int r = nums[i] - 1;
+            ans = max(ans, st.query(l, r) + 1);
+            st.update(nums[i], st.query(l, r) + 1);
+        }
+        return ans;
     }
 };
