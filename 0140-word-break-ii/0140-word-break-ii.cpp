aafc1036142a7d3593b43087b93b5bacf0feb588
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-01-21 11:56:56 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (99.28%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -6,24 +6,6 @@ public:
         }
         if(index == (int)s.size()) {
             bool ok = true;
-            // if(ans.size() != 4) {
-            //     return;
-            // }
-            // for(auto x: ans) {
-            //     // if(x.front() == '0' && x.size() != 1) {
-            //     //     ok = false;
-            //     //     break;
-            //     // }
-            //     // if(x.size() >= 4) {
-            //     //     ok = false;
-            //     //     break;
-            //     // }
-            //     // if(stoi(x) >= 0 && stoi(x) <= 255) {
-            //     //     continue;
-            //     // }
-            //     ok = false;
-            //     break;
-            // }
             if(ok) {
                 string str = "";
                 for(auto x: ans) {
