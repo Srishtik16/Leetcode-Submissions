985c1d2c21e7b6cfb36ff98a3e0f4df02a1c2ec3
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-02-02 21:02:25 +0530

    Time: 2215 ms (5.17%), Space: 661.1 MB (5.17%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,97 @@
+const int MaxN = 2e5 + 5;
+using ll = int;
+class SegmentTree {
+public:
+    unordered_map<int, int> st, lazy;
+    SegmentTree() {
+        st.clear();
+        lazy.clear();
+    }
+    // st[node] = ans for some range
+    ll merge(ll x, ll y) {
+        return max(x, y);
+    }
+    void propagate(ll node, ll start, ll end) {
+        if(lazy[node] == 0) {
+            return;
+        }
+        if(start == end) {
+            st[node] += lazy[node];
+            lazy[node] = 0;
+            return;
+        }
+        st[node] += lazy[node];
+        lazy[2 * node + 1] += lazy[node];
+        lazy[2 * node + 2] += lazy[node];
+        lazy[node] = 0;
+    }
+    void build(vector<int> &a, ll node, ll start, ll end) {
+        if(start == end) {
+            // Leaf node
+            st[node] = a[start];
+            return;
+        }
+        // Call for left and right child
+        ll mid = (start + end) / 2;
+        build(a, 2 * node + 1, start, mid);
+        build(a, 2 * node + 2, mid + 1, end);
+        st[node] = merge(st[2 * node + 1], st[2 * node + 2]);
+    }
+    ll query(ll node, ll start, ll end, ll l, ll r) {
+        propagate(node, start, end);
+        // If [start, end] is outside [l, r]
+        if(start > r || end < l) {
+            return 0; // Identity element
+        }
+        // [l [start, end] r]
+        else if(start >= l && end <= r) {
+            return st[node];
+        }
+        ll mid = (start + end) / 2;
+        ll leftAns = query(2 * node + 1, start, mid, l, r);
+        ll rightAns = query(2 * node + 2, mid + 1, end, l, r);
+        return merge(leftAns, rightAns);
+    }
+    void update(ll node, ll start, ll end, ll l, ll r, ll val) {
+        propagate(node, start, end);
+        // you are at that point
+        if(start >= l && end <= r) {
+            st[node] += val;
+            if(start != end) {
+                lazy[2 * node + 1] += val;
+                lazy[2 * node + 2] += val;
+            }
+            return;
+        }
+        // Completely outside
+        else if(start > r || end < l) {
+            return;
+        }
+        ll mid = (start + end) / 2;
+        update(2 * node + 1, start, mid, l, r, val);
+        update(2 * node + 2, mid + 1, end, l, r , val);
+        st[node] = merge(st[2 * node + 1], st[2 * node + 2]);
+    }
+};
+class MyCalendarTwo {
+public:
+    SegmentTree st;
+    MyCalendarTwo() {
+        ;
+    }
+    
+    bool book(int start, int end) {
+        int mx = st.query(0, 0, 1e9 + 5, start, end - 1);
+        if(mx == 2) {
+            return false;
+        }
+        st.update(0, 0, 1e9 + 5, start, end - 1, 1);
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
