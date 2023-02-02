b0181b87d8b1dc10703ed207fbe9528f809080e4
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-02-02 11:06:18 +0530

    Time: 6 ms (65.24%), Space: 9.4 MB (94.79%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        map<char, char> mapped;
+        array<char, 26> mapped;
         char ch = 'a';
         for(int i = 0; i < 26; i++) {
-            mapped[order[i]] = ch++;
+            mapped[order[i] - 'a'] = i;
         }
         for(auto &x: words) {
             for(char &c: x) {
-                c = mapped[c];
+                c = char('a' + mapped[c - 'a']);
             }
         }
         return is_sorted(words.begin(), words.end());
