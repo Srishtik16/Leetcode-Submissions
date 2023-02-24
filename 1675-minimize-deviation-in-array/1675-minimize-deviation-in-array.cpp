6843fae11e56ac6bb4fed60b959e05992a3433fa
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-02-24 14:35:54 +0530

    Time: 1495 ms (5.08%), Space: 251.4 MB (5.93%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -9,10 +9,10 @@ public:
         multiset<int> ms(nums.begin(), nums.end());
         int dev = INT_MAX;
         while(*ms.rbegin() % 2 == 0) {
-            dev = min(dev, abs(*ms.begin() - *ms.rbegin()));
             int x = *ms.rbegin();
+            dev = min(dev, abs(*ms.begin() - x));
             x /= 2;
-            ms.erase(ms.find(*ms.rbegin()));
+            ms.erase(ms.find(x * 2));
             ms.insert(x);
         }
         dev = min(dev, abs(*ms.begin() - *ms.rbegin()));
