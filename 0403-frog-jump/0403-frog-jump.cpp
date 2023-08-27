c83faae0d1f8bbd594f07b563585ee35e88a6178
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2023-08-27 13:56:53 +0530

    Time: 81 ms (81.11%), Space: 31.7 MB (67.65%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    map<int, bool> positions;
+    map<pair<int, int>, int> dp;
+    bool recurse(int index, int destination, int prevStep, int currStep) {
+        if(index == destination) {
+            return true;
+        }
+        if(index > destination) {
+            return false;
+        }
+        if(!positions[index]) {
+            return false;
+        }
+        if(index < 0) {
+            return false;
+        }
+        if(dp.find({index, currStep}) != dp.end()) {
+            return dp[{index, currStep}];
+        }
+        return dp[{index, currStep}] = (currStep > 0 ? recurse(index + currStep, destination, currStep, currStep) : false) || (currStep > 1 ? recurse(index + currStep - 1, destination, currStep, currStep - 1) : false) || (currStep > -1 ? recurse(index + currStep + 1, destination, currStep, currStep + 1) : false);
+    }
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        positions.clear();
+        for(auto x: stones) {
+            positions[x] = true;
+        }
+        return recurse(0, stones.back(), 0, 0);
+    }
+};
