8caaae0b6133abb21e334a0b20b7ebb6e8ece540
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2024-09-10 21:18:31 +0530

    Time: 65 ms (87.24%), Space: 10 MB (90.79%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int checkRecord(int n) {
-        // dp[i][j][k] = number of ways for i days, with atmost j trailing Ls, and atmost total k absents
-        // dp[i][j][k] = dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][2][k]
-        // dp[0][j][k] = 1, dp[1][j][k] = 1
+        // dp[i][j][k] = answer for first i days with atmost j trailing Ls and atmost k absents
+        // On ith day we have 3 choices
+        // A -> dp[i - 1][2][k - 1]
+        // L -> dp[i - 1][j - 1][k]
+        // P -> dp[i - 1][2][k]
         const int MOD = 1e9 + 7;
         int dp[n + 1][3][2];
         memset(dp, 0, sizeof(dp));
-        dp[0][0][0] = 1;
-        dp[0][0][1] = 1;
-        dp[0][1][0] = 1;
-        dp[0][1][1] = 1;
-        dp[0][2][1] = 1;
-        dp[0][2][0] = 1;
+        dp[0][0][0] = dp[0][1][0] = dp[0][2][0] = dp[0][0][1] = dp[0][1][1] = dp[0][2][1] = 1;
         for(int i = 1; i <= n; i++) {
             for(int j = 0; j < 3; j++) {
                 for(int k = 0; k < 2; k++) {
@@ -21,7 +18,7 @@ public:
                     if(j - 1 >= 0) {
                         dp[i][j][k] += dp[i - 1][j - 1][k];
                         dp[i][j][k] %= MOD;
-                    } 
+                    }
                     if(k - 1 >= 0) {
                         dp[i][j][k] += dp[i - 1][2][k - 1];
                         dp[i][j][k] %= MOD;
