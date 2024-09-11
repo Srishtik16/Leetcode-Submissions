220f14f277e4b255cfdd605c89f074c3ef9e8c9f
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2024-09-12 02:12:28 +0530

    Time: 394 ms (37.33%), Space: 148.9 MB (32.69%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -7,8 +7,9 @@ public:
         for(int i = 2; i < n; i++) {
             for(int j = 1; j < i; j++) {
                 int diff = arr[i] - arr[j];
-                if(lower_bound(arr.begin(), arr.begin() + j, diff) - arr.begin() < j && *lower_bound(arr.begin(), arr.begin() + j, diff) == diff) {
-                    dp[i][j] = max(dp[i][j], dp[j][lower_bound(arr.begin(), arr.begin() + j, diff) - arr.begin()] == 0 ? 3 : dp[j][lower_bound(arr.begin(), arr.begin() + j, diff) - arr.begin()] + 1);
+                int ind = lower_bound(arr.begin(), arr.begin() + j, diff) - arr.begin();
+                if(ind < j && arr[ind] == diff) {
+                    dp[i][j] = max(dp[i][j], dp[j][ind] == 0 ? 3 : dp[j][ind] + 1);
                     mx = max(mx, dp[i][j]);
                 }
             }
