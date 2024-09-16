f068202df97fb2d752907bf261c7749ddeebd943
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2024-09-16 20:36:49 +0530

    Time: 18 ms (16.81%), Space: 18.7 MB (5.42%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        auto convert = [&](string t) {
+            string hr = t.substr(0, 2);
+            string min = t.substr(3);
+            int hour = stoll(hr);
+            int mins = stoll(min);
+            int val = hour * 60 + mins;
+            return val;
+        };
+        auto clockWise = [&](int x, int y) {
+            return abs(x - y);
+        };
+        auto antiClockWise = [&](int x, int y) {
+            return min(x, y) + 1440 - (max(x, y));
+        };
+        vector<int> time1;
+        for(auto x: timePoints) {
+            time1.push_back(convert(x));
+        }
+        sort(time1.begin(), time1.end());
+        int ans = INT_MAX;
+        for(int i = 1; i < time1.size(); i++) {
+            ans = min(ans, clockWise(time1[i], time1[i - 1]));
+            ans = min(ans, antiClockWise(time1[i], time1[i - 1]));
+        }
+        ans = min(ans, clockWise(time1[0], time1.back()));
+        ans = min(ans, antiClockWise(time1[0], time1.back()));
+        return ans;
+    }
+};
