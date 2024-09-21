3f088e4d472f015a712228d8a3af816b7f46fdd9
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2024-09-22 03:11:23 +0530

    Time: 102 ms (30.71%), Space: 44.5 MB (19.69%) - LeetHub

--- 0975-odd-even-jump/0975-odd-even-jump.cpp
+++ 0975-odd-even-jump/0975-odd-even-jump.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int oddEvenJumps(vector<int>& arr) {
+        // dp[i][0] = can jump to index i as a high jump?
+        // dp[i][1] = can jump to index i as a low jump?
+        // dp[n - 1][0] = dp[n - 1][1] = true
+        // dp[i][0] = dp[lower_bound(arr[i]) -> index][1]
+        // dp[i][1] = dp[--upper_bound(arr[i]) -> index][0]
         int n = arr.size();
-        // dp[i][0/1] = move to index i via lower/higher(odd/even)move
         vector<vector<bool>> dp(n, vector<bool>(2, false));
-        dp[n - 1][0] = dp[n - 1][1] = true;
         map<int, int> m;
         m[arr.back()] = n - 1;
-        int ans = 1;
+        dp[n - 1] = {true, true};
         for(int i = n - 2; i >= 0; i--) {
             auto hi = m.lower_bound(arr[i]);
             auto lo = m.upper_bound(arr[i]);
@@ -15,10 +18,13 @@ public:
                 dp[i][1] = dp[hi -> second][0];
             }
             if(lo != m.begin()) {
-                dp[i][0] = dp[(--lo) -> second][1];
+                dp[i][0] = dp[prev(lo) -> second][1];
             }
-            ans += dp[i][1];
             m[arr[i]] = i;
+        }
+        int ans = 0;
+        for(auto x: dp) {
+            ans += x[1];
         }
         return ans;
     }
