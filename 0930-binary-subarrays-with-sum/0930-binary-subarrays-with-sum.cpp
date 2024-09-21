3518ce204b87b4ba8a1340a9ea73e2c5131771c6
Srishtik16 <51455021+Srishtik16@users.noreply.github.com>
2024-09-22 03:41:22 +0530

    Time: 76 ms (8.72%), Space: 41.9 MB (7.86%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -3,14 +3,10 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         map<int, int> m;
         int pref = 0, ans = 0;
+        m[0]++;
         for(auto x: nums) {
             pref += x;
-            if(pref == goal) {
-                ans++;
-            }
-            if(m.find(pref - goal) != m.end()) {
-                ans += m[pref - goal];
-            }
+            ans += m[pref - goal];
             m[pref]++;
         }
         return ans;
